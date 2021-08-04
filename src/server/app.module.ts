d30abae1fbc82814fa4f4e7b7a0c8e1bbe057a88
hobie2987 <hobie2987@gmail.com>
2021-08-03 21:41:56 -0400

    feat(app.module): remove graphql; enable csrf

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,17 +1,16 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
-import {AppController} from './app.controller';
+import { AppController } from './app.controller';
 import { CsrfMiddleware } from './middleware';
-import { DataModule } from './graphql/data.module';
 
 @Module({
-  imports: [DataModule],
+  imports: [],
   controllers: [AppController],
   providers: [],
 })
 export class AppModule implements NestModule {
 
   configure(consumer: MiddlewareConsumer): any {
-    // consumer.apply(CsrfMiddleware)
-    //     .forRoutes({ path: '*', method: RequestMethod.ALL })
+    consumer.apply(CsrfMiddleware)
+        .forRoutes({ path: '*', method: RequestMethod.ALL })
   }
 }
