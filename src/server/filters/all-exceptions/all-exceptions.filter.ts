0a8b4e3804203f0061cca3f5b79484319987e3ad
hobie2987 <hobie2987@gmail.com>
2021-09-03 16:16:34 -0400

    feat(filter): add exception filter

---
+++ src/server/filters/all-exceptions/all-exceptions.filter.ts
@@ -0,0 +1,30 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+import { LoggerService } from '../../services';
+import { LogCode } from '../../../types';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private logger: LoggerService) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    this.logger.error({
+      message: 'Unhandled exception',
+      feature: 'all-exceptions.filter',
+      error: exception,
+      url: request.originalUrl,
+      method: request.method,
+      code: LogCode.EXCEPTION
+    });
+
+    response.sendStatus(status);
+  }
+}
