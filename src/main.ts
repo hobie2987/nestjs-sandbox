d07ac56181f8e5ef706d03314a81555e5541c245
hobie2987 <hobie2987@gmail.com>
2021-09-03 16:13:11 -0400

    feat(main): add exception handler

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './server/app.module';
+import { LoggerService } from './server/services';
+import { AllExceptionsFilter } from './server/filters';
 import * as cookieParser from 'cookie-parser';
 import * as helmet from 'helmet';
 import * as nocache from 'nocache';
 import * as compression from 'compression';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = app.get(LoggerService)
+  app.useGlobalFilters(new AllExceptionsFilter(logger));
   app.use(cookieParser());
   app.use(nocache());
   app.use(helmet());
