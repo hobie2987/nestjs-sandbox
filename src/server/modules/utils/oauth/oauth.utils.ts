a232a96dd26c57fd32a2161171cc3ad6132d363b
hobie2987 <hobie2987@gmail.com>
2021-11-23 06:57:57 -0500

    feat(oauth): create oauth utils

---
+++ src/server/modules/utils/oauth/oauth.utils.ts
@@ -0,0 +1,34 @@
+import { AsyncSubject, firstValueFrom, interval } from 'rxjs';
+import crypto from 'crypto';
+import { wait } from '@utils';
+
+class OAuthUtils {
+  readonly token: AsyncSubject<string>;
+
+  constructor() {
+    this.token = new AsyncSubject<string>();
+    this.generateToken();
+    this.renewToken();
+  }
+
+  public async getToken(): Promise<string> {
+    return firstValueFrom(this.token);
+  }
+
+  private generateToken(): void {
+    wait(500).then(() => {
+      const token = crypto.randomBytes(305).toString('base64');
+      this.token.next(token);
+      this.token.complete();
+    });
+  }
+
+  private renewToken(): void {
+    interval(10000).subscribe(() => {
+      this.generateToken();
+    });
+  }
+}
+
+const instance = new OAuthUtils();
+export { instance as OAuthUtils };
