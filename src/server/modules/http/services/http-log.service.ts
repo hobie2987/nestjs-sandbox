eb285506348ea55717dfaacdcdf0e5f342cd453b
hobie2987 <hobie2987@gmail.com>
2021-12-03 10:10:28 -0500

    feat(http): rename to onRequest onResponse

--- src/server/modules/http/services/http-log.service.ts
+++ src/server/modules/http/services/http-log.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { LoggerService, LogLevel, LogCode, LogMessage } from '../../logger';
+import { LoggerService, LogLevel, LogCode, LogMessage } from '@logger';
 import { AxiosInstance } from 'axios';
 
 @Injectable()
@@ -10,15 +10,15 @@ export class HttpLogService {
   }
 
   private setInterceptors(axiosRef: AxiosInstance): void {
-    axiosRef.interceptors.request.use(this.setStartTime);
-    axiosRef.interceptors.response.use(this.logResponse);
+    axiosRef.interceptors.request.use(this.onRequest);
+    axiosRef.interceptors.response.use(this.onResponse);
   }
 
-  setStartTime = (config: any): void => {
+  private onRequest = (config: any): void => {
     return { ...config, startTime: Date.now() };
   };
 
-  logResponse = (response: any): void => {
+  private onResponse = (response: any): void => {
     const { message, statusText, config, code, status, isAxiosError } =
       response;
     const isError = response instanceof Error || isAxiosError;
