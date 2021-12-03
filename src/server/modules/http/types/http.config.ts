39ef328b4b8af4ca8c6559a239fa18a7e511d885
hobie2987 <hobie2987@gmail.com>
2021-12-03 10:09:24 -0500

    feat(http): update timeout message

--- src/server/modules/http/types/http.config.ts
+++ src/server/modules/http/types/http.config.ts
@@ -2,7 +2,7 @@ import { HttpModuleOptions } from '@nestjs/axios';
 
 export const HttpConfig: HttpModuleOptions = {
   timeout: 10000, // 10 seconds
-  timeoutErrorMessage: 'Request Timeout',
+  timeoutErrorMessage: 'Request timed out after 10 seconds',
   xsrfCookieName: 'XSRF-TOKEN',
   xsrfHeaderName: 'X-XSRF-TOKEN',
   method: 'GET',
