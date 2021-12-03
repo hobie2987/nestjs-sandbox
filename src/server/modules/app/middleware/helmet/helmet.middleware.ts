76b909d94843070f463b4cdcf37cf7ffa66de49b
hobie2987 <hobie2987@gmail.com>
2021-12-03 10:10:58 -0500

    feat(helmet): helmet middleware config wrapper

---
+++ src/server/modules/app/middleware/helmet/helmet.middleware.ts
@@ -0,0 +1,22 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import helmet from 'helmet';
+
+@Injectable()
+export class HelmetMiddleware implements NestMiddleware {
+  use = helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [
+          `'self'`,
+          `'unsafe-inline'`,
+          'cdn.jsdelivr.net',
+          'fonts.googleapis.com',
+        ],
+        fontSrc: [`'self'`, 'fonts.gstatic.com'],
+        imgSrc: [`'self'`, 'data:', 'cdn.jsdelivr.net'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`, `cdn.jsdelivr.net`],
+      },
+    },
+  });
+}
