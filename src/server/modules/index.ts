e0b79647dffd39135b1b47e26cbbf555deb88277
hobie2987 <hobie2987@gmail.com>
2021-12-03 10:07:40 -0500

    feat(modules): export http module

--- src/server/modules/index.ts
+++ src/server/modules/index.ts
@@ -1,4 +1,5 @@
+export * from './app/app.module';
 export * from './graphql/gql.module';
-// export * from './http/http.module';
+export * from './http/http.module';
 export * from './logger/logger.module';
 export * from './xsrf/xsrf.module';
