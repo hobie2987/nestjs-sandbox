e2a55afb90e30636b2a664943b9d8930a562d891
hobie2987 <hobie2987@gmail.com>
2021-12-20 14:32:25 -0500

    feat(certs): generate only when !exist

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -1,7 +1,15 @@
+KEY=dist/server.key
+CRT=dist/server.crt
+
 if command -v openssl > /dev/null
 then
-  mkdir -p dist
-  openssl req -new -x509 -nodes -days 365 -keyout dist/server.key -out dist/server.crt -subj "//O=/C=US/CN=localhost"
+    if [ ! -e $KEY ] || [ ! -e $CRT ]
+    then
+      mkdir -p dist
+      openssl req -new -x509 -nodes -days 365 -keyout $KEY -out $CRT -subj "//O=/C=US/CN=localhost"
+    else
+      echo Certs exist... Skipping cert generation
+    fi
 else
   echo unknown command: openssl
 fi
