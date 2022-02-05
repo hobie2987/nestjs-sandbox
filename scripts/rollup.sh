067fb51cc47e71c251c3b33335c75c5c41de5e65
hobie2987 <hobie2987@gmail.com>
2022-02-05 11:38:33 -0500

    feat(rollup): rollup vendor code

--- scripts/rollup.sh
+++ scripts/rollup.sh
@@ -1,12 +1,14 @@
 #Download source files
 mkdir -p temp/
-curl 'https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js' -o temp/js.cookie.js
-curl 'https://polyfill.io/v3/polyfill.min.js?features=default&flags=always,gated' -o temp/polyfill.js
+curl 'https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.js' -o temp/js.cookie.js
+curl 'https://polyfill.io/v3/polyfill.js?features=default&flags=always,gated' -o temp/polyfill.js
 
 
 #Combine all js files in temp > vendor.js
 mkdir -p dist/assets
 cat temp/*.js > dist/assets/vendor.js
+
+minify dist/assets/vendor.js > dist/assets/vendor.min.js
 
 #delete temp directory
 rm -f -r temp
